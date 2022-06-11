f535f93a19042ee18e0d0276ec207bf52583fec3
Ryan <parbattamang1234@gmail.com>
2022-06-11 13:10:10 +0545

    chat.php

--- chat.php
+++ chat.php
@@ -26,13 +26,14 @@
           <p><?php echo $row['status']; ?></p>
         </div>
       </header>
+      <span class="bi bi-three-dots-vertical"></span>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
       <div class="chat-box">
-
-      </div>
+        </div>
       <form action="#" class="typing-area">
         <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
         <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
-        <button><i class="fab fa-telegram-plane"></i></button>
+        <button><i class="fa-solid fa-paper-plane-top"></i></button>
       </form>
     </section>
   </div>
